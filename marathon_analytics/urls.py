7be89a569c89d7be90dcdd3f6bec66ee032a1d26
kennajw <kennajw@bu.edu>
2024-11-11 16:36:30 -0500

    marathon analytics example

--- marathon_analytics/urls.py
+++ marathon_analytics/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', ResultsListView.as_view(), name="home"),
-    path('results', ResultsListView.as_view(), name="results"),
+    path(r'', ResultsListView.as_view(), name="home"),
+    path(r'results', ResultsListView.as_view(), name="results"),
+    path(r'result/<int:pk>', ResultDetailView.as_view(), name='result_detail'),
 ]
